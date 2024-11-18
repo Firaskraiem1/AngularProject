cf8aa8a119ca2431faefe0ed2359b5954c30245b
Firaskraiem1 <firasbenkraiem8@gmail.com>
2024-11-18 12:06:55 +0100

    workshop1 end

--- src/app/Residences/residences/residences.component.ts
+++ src/app/Residences/residences/residences.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Residence } from 'src/app/Core/Models/residence';
 
 @Component({
@@ -7,6 +7,7 @@ import { Residence } from 'src/app/Core/Models/residence';
   styleUrls: ['./residences.component.css']
 })
 export class ResidencesComponent {
+
   listResidences: Residence[] = [
     {
       id: 1, "name": "El fel", "address": "Borj Cedria",
@@ -27,7 +28,7 @@ export class ResidencesComponent {
       "image": "../../assets/images/R4.jpeg", status: "En Construction"
     }
   ];
-  
+
   residenceSelected!: Residence;
   showLocation(residence: Residence): void {
     if (residence.address !== "inconnu") {
@@ -36,5 +37,20 @@ export class ResidencesComponent {
       alert("L'adresse de résidence " + residence.name + " est inconnu");
     }
   }
-
+  liked: boolean = false
+  favoris!: Residence[];
+  like(r: Residence) {
+    this.liked = !this.liked;
+    this.residenceSelected = r;
+    if (this.liked) {
+      this.favoris.push(r);
+    } else {
+      this.favoris = this.favoris.filter(res => res.id != r.id);
+    }
+  }
+  filtredResidences: Residence[] = this.listResidences;
+  filterAddress: string = "";
+  filter() {
+    this.filtredResidences = this.listResidences.filter(r => r.address.toLowerCase().includes(this.filterAddress.toLowerCase()));
+  }
 }
