1013cfb827aa7feb0edafd29d74a61a3b3f942b5
Firaskraiem1 <firasbenkraiem8@gmail.com>
2024-12-08 23:59:56 +0100

    last commit

--- src/app/Residences/add-residence/add-residence.component.ts
+++ src/app/Residences/add-residence/add-residence.component.ts
@@ -1,6 +1,6 @@
 import { group } from '@angular/animations';
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Residence } from 'src/app/Core/Models/residence';
 
 @Component({
@@ -11,14 +11,14 @@ import { Residence } from 'src/app/Core/Models/residence';
 export class AddResidenceComponent {
 
   constructor(private fb: FormBuilder) { }
+  imageError: string | null = null;
   newResidence!: Residence
   addResidence = this.fb.group({
-    id: [''],
     name: ['', [Validators.required, Validators.minLength(3)]],
     address: ['', Validators.required],
     image: ['', Validators.required],
     status: ['', Validators.required],
-    apartments: ['', Validators.required]
+    apartments: this.fb.array([])
   });
   statusList: String[] = [" Disponible", "En_Construction", "Vendu"];
   get id() { return this.addResidence.get('id'); }
@@ -26,6 +26,26 @@ export class AddResidenceComponent {
   get address() { return this.addResidence.get('address'); }
   get image() { return this.addResidence.get('image'); }
   get status() { return this.addResidence.get('status'); }
+  get apartments() {
+    return (this.addResidence.get('apartments') as FormArray);
+  }
+  onFileSelect(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      const file = fileInput.files[0];
+      if (this.isValidImage(file)) {
+        this.addResidence.patchValue({ image: file.name });
+        this.imageError = null;
+      } else {
+        this.imageError = 'Invalid image format. Only JPG or PNG allowed.';
+      }
+    }
+  }
+
+  isValidImage(file: File): boolean {
+    const validFormats = ['image/jpeg', 'image/png'];
+    return validFormats.includes(file.type);
+  }
   add() {
     // console.log(this.addResidence)
     // this.newResidence.address = String(this.addResidence.controls.address);
@@ -33,4 +53,16 @@ export class AddResidenceComponent {
     this.newResidence = this.addResidence.value as unknown as Residence;
     console.log(this.newResidence);
   }
+
+  addApartment() {
+    this.apartments.push(this.fb.group({
+      apartNum: ['', Validators.required],
+      floorNum: ['', Validators.required],
+      surface: ['', Validators.required],
+      terrace: ['', Validators.required],
+      surfaceterrace: ['', Validators.required],
+      category: ['', Validators.required],
+    }));
+  }
+
 }
